ab5dcc682575f1bf971c77054e5028189cb1c947
namoray <152790419+namoray@users.noreply.github.com>
2024-09-04 15:26:13 +0100

    docs fix (#85)

--- autoupdate_validator_steps.sh
+++ autoupdate_validator_steps.sh
@@ -21,5 +21,5 @@
 #!/bin/bash
 
 # Function to run a command with sudo only if it is available
-./launch_validators.sh --without-self-heal
+# ./launch_validators.sh --without-self-heal
 echo "Autoupdate steps complete :)"
